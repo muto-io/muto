91ebdf5b2498d1b911a72d319d6ee77ba8ef480a
I539231 <pascal.zimmermann01@sap.com>
2026-07-11 23:48:51 +0200

    feat(messaging): add Kafka (Sarama) MessageBus implementation

---
+++ core/messaging/kafka/kafka.go
@@ -0,0 +1,78 @@
+package kafka
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/IBM/sarama"
+	"github.com/muto-io/muto/core/agent"
+	"github.com/muto-io/muto/core/messaging"
+)
+
+func init() {
+	messaging.Register(messaging.BusTypeKafka, func(cfg *messaging.Config) (messaging.MessageBus, error) {
+		return New(cfg)
+	})
+}
+
+type KafkaBus struct {
+	producer sarama.SyncProducer
+	consumer sarama.Consumer
+	brokers  []string
+}
+
+func New(cfg *messaging.Config) (*KafkaBus, error) {
+	scfg := sarama.NewConfig()
+	scfg.Producer.Return.Successes = true
+	if cfg.Username != "" {
+		scfg.Net.SASL.Enable = true
+		scfg.Net.SASL.User = cfg.Username
+		scfg.Net.SASL.Password = cfg.Password
+	}
+	producer, err := sarama.NewSyncProducer(cfg.URLs, scfg)
+	if err != nil {
+		return nil, fmt.Errorf("kafka producer: %w", err)
+	}
+	consumer, err := sarama.NewConsumer(cfg.URLs, scfg)
+	if err != nil {
+		producer.Close()
+		return nil, fmt.Errorf("kafka consumer: %w", err)
+	}
+	return &KafkaBus{producer: producer, consumer: consumer, brokers: cfg.URLs}, nil
+}
+
+func (b *KafkaBus) Publish(ctx context.Context, topic string, msg []byte) error {
+	_, _, err := b.producer.SendMessage(&sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.ByteEncoder(msg),
+	})
+	return err
+}
+
+func (b *KafkaBus) Subscribe(ctx context.Context, topic string, handler agent.MsgHandler) error {
+	pc, err := b.consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+	if err != nil {
+		return fmt.Errorf("kafka subscribe: %w", err)
+	}
+	go func() {
+		for {
+			select {
+			case msg, ok := <-pc.Messages():
+				if !ok {
+					return
+				}
+				_ = handler(msg.Topic, msg.Value)
+			case <-ctx.Done():
+				pc.Close()
+				return
+			}
+		}
+	}()
+	return nil
+}
+
+func (b *KafkaBus) Close() error {
+	b.producer.Close()
+	b.consumer.Close()
+	return nil
+}
--- go.mod
+++ go.mod
@@ -14,3 +14,22 @@ require (
 	sigs.k8s.io/controller-runtime v0.18.0
 	sigs.k8s.io/controller-tools v0.16.0
 )
+
+require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/eapache/go-resiliency v1.7.0 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.18.6 // indirect
+	github.com/nats-io/nkeys v0.4.15 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/pierrec/lz4/v4 v4.1.27 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20250401214520-65e299d6c5c9 // indirect
+	golang.org/x/crypto v0.53.0 // indirect
+	golang.org/x/net v0.56.0 // indirect
+	golang.org/x/sys v0.46.0 // indirect
+)
